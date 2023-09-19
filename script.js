7cede88255b9f1b08a8824b6f1956ec0b4dc532d
marcieleabreu <137800208+marcieleabreu@users.noreply.github.com>
2023-09-19 11:47:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+calcule.addEventListener('click',function(){
+
+    var a = valorA.value;
+    var b = valorB.value;
+
+    a= parseInt(a);
+    b= parseInt(b);
+
+    var result= a + b;
+
+    alert (result);
+
+
+});
+
+
